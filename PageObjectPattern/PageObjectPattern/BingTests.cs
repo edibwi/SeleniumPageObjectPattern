bff429d97c30c02fcbc60bbe66921237890bd8df
Edi Prayitno <its.edi@gmail.com>
2016-12-09 09:03:32 +0800

    using Page Object of Selenium Web Driver

---
+++ PageObjectPattern/PageObjectPattern/BingTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.PageObjects;
+using OpenQA.Selenium.Support.UI;
+using PageObjectPattern.PageObjects;
+
+namespace PageObjectPattern
+{
+    [TestClass]
+    public class BingTests
+    {
+        public IWebDriver Driver { get; set; }
+        public WebDriverWait Wait { get; set; }
+
+        [TestInitialize]
+        public void SetupTest()
+        {
+            this.Driver = new ChromeDriver();
+            this.Wait = new WebDriverWait(this.Driver, TimeSpan.FromSeconds(30));
+        }
+
+        [TestCleanup]
+        public void TeardownTest()
+        {
+            this.Driver.Quit();
+        }
+
+        [TestMethod]
+        public void SearchTextInBing_First()
+        {
+            BingMainPage bingMainPage = new BingMainPage(this.Driver);
+            bingMainPage.Navigate();
+            bingMainPage.Search("Automate The Planet");
+            bingMainPage.ValidateResultsCount("2.760.000 RESULTS");
+        }
+    }
+}
